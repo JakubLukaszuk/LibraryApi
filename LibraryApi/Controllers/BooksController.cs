cc8268c2d307cc50e9049996484b53c07ef253e9
JakubLukaszuk <41912745+JakubLukaszuk@users.noreply.github.com>
2019-12-25 02:15:34 +0100

    Update Books Controller to full REST
    Books Controller updated with PUT, DELETE, POST methods.

--- LibraryApi/Controllers/BooksController.cs
+++ LibraryApi/Controllers/BooksController.cs
@@ -14,10 +14,16 @@ namespace LibraryApi.Controllers
     public class BooksController : Controller
     {
         private IBookRepository _bookRepository;
+        private ICategoryRepository _categoryRepository;
+        private IAuthorRepository _authorRepository;
+        private IReviewRepository _reviewRepository;
 
-        public BooksController(IBookRepository bookRepository)
+        public BooksController(IBookRepository bookRepository, ICategoryRepository categoryRepository, IAuthorRepository authorRepository, IReviewRepository reviewRepository)
         {
             _bookRepository = bookRepository;
+            _categoryRepository = categoryRepository;
+            _authorRepository = authorRepository;
+            _reviewRepository = reviewRepository;
         }
 
         [HttpGet]
@@ -45,7 +51,7 @@ namespace LibraryApi.Controllers
             return Ok(booksDtoList);
         }
 
-        [HttpGet("{bookId}")]
+        [HttpGet("{bookId}", Name = "GetBook")]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
         [ProducesResponseType(200, Type = typeof(BookDataTransferObjects))]
@@ -88,5 +94,140 @@ namespace LibraryApi.Controllers
             return Ok(rating);
         }
 
+        //api/books?authId=1&authId=2&catId=1&catId=2
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Book))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult CreateBook([FromQuery] List<int> authIds, [FromQuery] List<int> categoryIds, [FromBody] Book bookToCreate)
+        {
+            StatusCodeResult statusCode = ValdateBook(authIds, categoryIds, bookToCreate);
+
+            if (ModelState.IsValid)
+                return StatusCode(statusCode.StatusCode);
+
+            if (_bookRepository.CreateBook(bookToCreate, authIds, categoryIds))
+            {
+                ModelState.AddModelError("", $"Somethink went wrong savving book");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetBook", new { bookId = bookToCreate.Id }, bookToCreate);
+        }
+
+        //api/books/bookId?authId=1&authId=2&catId=1&catId=2
+        [HttpPut("{bookId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult UpdateBook(int bookId, [FromQuery] List<int> authIds, [FromQuery] List<int> categoryIds, [FromBody] Book bookToUpdate)
+        {
+            StatusCodeResult statusCode = ValdateBook(authIds, categoryIds, bookToUpdate);
+
+            if (bookId == bookToUpdate.Id)
+                return BadRequest();
+
+            if (!_bookRepository.IsBookExist(bookId))
+                return NotFound();
+
+            if (ModelState.IsValid)
+                return StatusCode(statusCode.StatusCode);
+
+            if (_bookRepository.UpdateBook(bookToUpdate, authIds, categoryIds))
+            {
+                ModelState.AddModelError("", $"Somethink went wrong updating book");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetBook", new { bookId = bookToUpdate.Id }, bookToUpdate);
+        }
+
+        [HttpDelete("{bookId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult DeleteReviewer(int bookId)
+        {
+            if (!_bookRepository.IsBookExist(bookId))
+            {
+                return BadRequest(ModelState);
+            }
+
+            var reviewsToDelete = _reviewRepository.GetReviewsOfBook(bookId);
+            var bookToDelete = _bookRepository.GetBook(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+            if (!_reviewRepository.DeleteReviews(reviewsToDelete.ToList()))
+            {
+                ModelState.AddModelError("", "Somethink went wrong. Reviews not deleted");
+                return StatusCode(500, ModelState);
+            }
+
+            if (_bookRepository.DeleteBook(bookToDelete))
+            {
+                ModelState.AddModelError("", $"Somethink went wrong. Book: {bookToDelete.Title} not deleted");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
+
+        private StatusCodeResult ValdateBook(List<int> authId, List<int> categoryIds, Book books)
+        {
+            if (books == null)
+            {
+                ModelState.AddModelError("", "Missing book");
+                return BadRequest();
+            }
+            else if(authId.Count() <= 0)
+            {
+                ModelState.AddModelError("", "Missing author id");
+                return BadRequest();
+            }
+            else if(categoryIds.Count() <= 0)
+            {
+                ModelState.AddModelError("", "Missing category id");
+                return BadRequest();
+            }
+            
+            if(_bookRepository.IsDuplicateISBN(books.Isbn, books.Id))
+            {
+                ModelState.AddModelError("", "Duplicate ISBN");
+                return StatusCode(422);
+            }
+
+            foreach (var id in authId)
+            {
+                if (!_authorRepository.IsAuthorExist(id))
+                {
+                    ModelState.AddModelError("", "Author Not Found");
+                    return StatusCode(404);
+                }
+            }
+
+
+            foreach (var id in categoryIds)
+            {
+                if (!_categoryRepository.IsCategoryExist(id))
+                {
+                    ModelState.AddModelError("", "Category Not Found");
+                    return StatusCode(404);
+                }
+            }
+
+            if(ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Critical Error");
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
     }
 }
